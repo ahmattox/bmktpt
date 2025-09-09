bc02f2622a9187cddef6a8fa38d378448792b413
Anthony Mattox <anthony@friendsoftheweb.com>
2025-09-09 16:17:44 -0400

    Updates Cube Tuesday Info

--- src/components/cube-menu/TuesdayNightCube.tsx
+++ src/components/cube-menu/TuesdayNightCube.tsx
@@ -10,14 +10,6 @@ export const TuesdayNightCube: React.FC = () => {
 
   return (
     <div className={styles.container}>
-      <div className={styles.notice}>
-        <h3>Temporary Location Change</h3>
-        <p>
-          We&rsquo;ll be organizing in a nearby location while NLB is relocating
-          to their new space. Check the discord for updates!
-        </p>
-      </div>
-
       <p>
         We draft{' '}
         <a href="http://luckypaper.co/what-is-a-cube/">
@@ -28,21 +20,21 @@ export const TuesdayNightCube: React.FC = () => {
         <a href="https://www.nolandbaltimore.com">
           No Land Beyond
           <OffsiteIcon />
-        </a>
-        . All players are welcome.
+        </a>{' '}
+        (recently moved to 108 W North Avenue) . All players are welcome.
       </p>
 
       {showDetails ? (
         <>
           <p>
-            Drafts start promptly at 6:30. No sign ups are required. Arrive
-            early if you can (or ping someone in the discord) so we can organize
-            pods according to the number of players.
+            Drafts start promptly at 6:30. No sign ups are required. Arrive a
+            little early so we can organize pods according to the number of
+            players.
           </p>
 
           <p>
-            Be sure to support our great LGS! Buy a drink, Magic supplies, or
-            game pass so they can keep the literal lights on.
+            Be sure to support our great LGS! NLB charges a $5 cover for table
+            space. They also have a bar and serve food.
           </p>
 
           <p>
